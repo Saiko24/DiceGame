0555de2cea4ae4dd34010c1c032e14792bc8ec80
vitn24 <49666347+vitn24@users.noreply.github.com>
2019-04-23 21:55:06 +0300

    Add files via upload

--- RollDice.cs
+++ RollDice.cs
@@ -9,10 +9,33 @@ public class RollDice : MonoBehaviour {
 
     public PlayerCurrentParameter movesParameter;
     public Animator animator;
+    public BoxCollider2D buttonCollider;
+    public Image blockSprite;
 
-    public void randomDiceRoll()
+    private void Start()
+    {
+        movesParameter.currentParameterValue = 0;
+    }
+
+    public void RandomDiceRoll()
     {
         animator.Play("RollDiceAnimation");
         movesParameter.ParameterValue = Random.Range(1, 7);
+        BlockDiceRoll();
+    }
+
+    public void BlockDiceRoll()
+    {
+        if (buttonCollider.enabled == true)
+        {
+            buttonCollider.enabled = false;
+            blockSprite.enabled = true;
+        }
+        else
+        {
+            buttonCollider.enabled = true;
+            blockSprite.enabled = false;
+        }
+
     }
 }
