106ab6bcb1beee518ebe2553c0c85cd42ad73117
vitn24 <49666347+vitn24@users.noreply.github.com>
2019-04-23 21:53:47 +0300

    Add files via upload

--- FillBattleMap.cs
+++ FillBattleMap.cs
@@ -15,10 +15,20 @@ public class FillBattleMap : MonoBehaviour {
     public IntArray2d effectsMapIntArray;
     public IntArray2d creaturesMapIntArray;
     public GameObjectVariable PlayerBattleCell;
+    public GameObjectList enemyBattleCell;
+    public EnemiesArray enemiesArray;
     public PlayerBattle player;
+    public EnemyBattle enemy;
+    public int enemyCount = 2;
+    public int obstacleCount = 6;
     public GameEvent movePlayer;
     public GridLayoutGroup gridLayoutGroup;
-    public enum battleCellType { Empty, DefaultFloor, ObstacleWall }
+    public enum battleCellType { Empty, DefaultFloor, ObstacleWall, EnemyOnCell }
+
+    private void Awake()
+    {
+        enemyBattleCell.gameObjectList.Clear();
+    }
 
     // Use this for initialization
     void Start () {
@@ -28,13 +38,17 @@ public class FillBattleMap : MonoBehaviour {
         effectsMapIntArray.array = new int[battleMapHeight.value, battleMapWidth.value];
         battleCellArray.cellGameObjectArray = new GameObject[battleMapHeight.value, battleMapWidth.value];
 
+        enemyCount = 2;
+
         CellsSpawn();
         PlayerSpawn();
-        ObstacleSpawn();
         EnemySpawn();
         EffectsSpawn();
+        ObstacleSpawn();
+
     }
 	
+
     public void CellsSpawn()
     {
         for (int i = 0; i < battleMapHeight.value; i++)
@@ -71,27 +85,50 @@ public class FillBattleMap : MonoBehaviour {
 
         PlayerBattleCell.value = this.battleCellArray.cellGameObjectArray[battleMapHeight.value - playerHeight, battleMapWidth.value - playerWidth];
         PlayerBattleCell.value.GetComponent<BattleCell>().playerOnCell = true;
+        PlayerBattleCell.value.GetComponent<BattleCell>().walkable = true;
         playerGameObj.transform.position = PlayerBattleCell.value.transform.position;
         playerGameObj.GetComponent<PlayerBattle>().playerHeightIndex = battleMapHeight.value - playerHeight;
         playerGameObj.GetComponent<PlayerBattle>().playerWidthIndex = battleMapWidth.value - playerWidth;
     }
 
+    public void EnemySpawn()
+    {
+        enemiesArray.array.Clear();
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            int enemyHeight = Random.Range(1, battleMapHeight.value);
+            int enemyWidth = Random.Range(1, battleMapWidth.value);
+
+            creaturesMapIntArray.array[battleMapHeight.value - enemyHeight, battleMapWidth.value - enemyWidth] = 2;
+            battleCellArray.cellGameObjectArray[battleMapHeight.value - enemyHeight, battleMapWidth.value - enemyWidth].GetComponent<BattleCell>().enemyOnCell = true;
+
+            GameObject enemyGameObj = Instantiate(enemy.gameObject) as GameObject;
+            enemyGameObj.transform.SetParent(creaturesContainer.transform, false);
+
+            enemyGameObj.GetComponent<EnemyBattle>().enemyHeightIndex = battleMapHeight.value - enemyHeight;
+            enemyGameObj.GetComponent<EnemyBattle>().enemyWidthIndex = battleMapWidth.value - enemyWidth;
+            enemiesArray.array.Add(enemyGameObj.GetComponent<EnemyBattle>());
+
+            enemyBattleCell.gameObjectList.Add(this.battleCellArray.cellGameObjectArray[battleMapHeight.value - enemyHeight, battleMapWidth.value - enemyWidth]);
+            enemyBattleCell.gameObjectList[i].GetComponent<BattleCell>().enemyOnCell = true;
+            enemyBattleCell.gameObjectList[i].GetComponent<BattleCell>().walkable = false;
+            enemyGameObj.transform.position = enemyBattleCell.gameObjectList[i].transform.position;
+        }
+    }
+
     public void ObstacleSpawn()
     {
-        int g = 0;
-
-        for (int i = 0; g < 6; i++)
+        for (int i = 0; i < obstacleCount; i++)
         {
-            Debug.Log("obs");
-            int randI = Random.Range(0, battleMapHeight.value);
-            int randY = Random.Range(0, battleMapWidth.value);
+            int randH = Random.Range(0, battleMapHeight.value);
+            int randW = Random.Range(0, battleMapWidth.value);
 
-            if (randI != PlayerBattleCell.value.GetComponent<BattleCell>().heightIndex || randY != PlayerBattleCell.value.GetComponent<BattleCell>().widthIndex)
+            if (randH != PlayerBattleCell.value.GetComponent<BattleCell>().heightIndex || randW != PlayerBattleCell.value.GetComponent<BattleCell>().widthIndex)
             {
-                g++;
-                Debug.Log("1");
-                battleCellArray.cellGameObjectArray[randI, randY].GetComponent<BattleCell>().DisplayBattleCellInfo(battleCellType.ObstacleWall.ToString(), randI, randY);
+                battleCellArray.cellGameObjectArray[randH, randW].GetComponent<BattleCell>().DisplayBattleCellInfo(battleCellType.ObstacleWall.ToString(), randH, randW);
             }
+
         }
     }
 }
